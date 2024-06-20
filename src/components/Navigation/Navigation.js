bd88c2e98a241962700d98ae40ecef037a04fe8c
Pancrass Njoroge <wankenobi29@gmail.com>
2024-06-20 23:09:28 +0300

    Setup & First page

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+import 'tachyons';
+
+const Navigation = () =>  {
+    return (
+        <nav style={{display: 'flex', justifyContent: 'space-between'}}>
+            <p className="tr f3 link dim black b u">Sign Out</p>
+        </nav>
+    )
+}
+
+
+export default Navigation;
